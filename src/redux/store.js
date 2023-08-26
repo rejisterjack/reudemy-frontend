e16670ea434c61d435a2bc4073c26538e4f5532c
Rupam Das <rejisterjack@gmail.com>
2023-08-26 15:56:06 +0530

    link

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,5 +20,4 @@ const store = configureStore({
 
 export default store;
 
-// export const server = "http://localhost:8000/api/v1"
-export const server = "https://reudemy-server.onrender.com/api/v1"
+export const server = process.env.REACT_APP_BACKEND_URI

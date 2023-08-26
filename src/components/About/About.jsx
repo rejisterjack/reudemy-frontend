c2ac8a73e3c75f6e6dd55164006390833dda683c
Rupam Das <rejisterjack@gmail.com>
2023-08-26 16:13:16 +0530

    changed logos

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,33 +8,31 @@ import {
   Stack,
   Text,
   VStack,
-} from '@chakra-ui/react';
-import React from 'react';
-import { RiSecurePaymentFill } from 'react-icons/ri';
-import { Link } from 'react-router-dom';
-import introVideo from '../../assets/videos/intro.mp4';
-import termsAndCondition from '../../assets/docs/termsAndCondition';
+} from "@chakra-ui/react"
+import React from "react"
+import { RiSecurePaymentFill } from "react-icons/ri"
+import { Link } from "react-router-dom"
+import introVideo from "../../assets/videos/intro.mp4"
+import termsAndCondition from "../../assets/docs/termsAndCondition"
+import logo from "../../assets/images/reudemy.png"
 
 const Founder = () => (
-  <Stack direction={['column', 'row']} spacing={['4', '16']} padding={'8'}>
+  <Stack direction={["column", "row"]} spacing={["4", "16"]} padding={"8"}>
     <VStack>
-      <Avatar
-        src="https://avatars.githubusercontent.com/u/25058652"
-        boxSize={['40', '48']}
-      />
+      <Avatar src={logo} boxSize={["40", "48"]} />
       <Text children="Co-Founder" opacity={0.7} />
     </VStack>
 
-    <VStack justifyContent={'center'} alignItems={['center', 'flex-start']}>
-      <Heading children="Abhishek Singh" size={['md', 'xl']} />
+    <VStack justifyContent={"center"} alignItems={["center", "flex-start"]}>
+      <Heading children="rejisterjack" size={["md", "xl"]} />
       <Text
-        textAlign={['center', 'left']}
+        textAlign={["center", "left"]}
         children={`Hi, I am a full-stack developer and a teacher.
       Our mission is to provide quality content at reasonable price.`}
       />
     </VStack>
   </Stack>
-);
+)
 
 const VideoPlayer = () => (
   <Box>
@@ -49,46 +47,46 @@ const VideoPlayer = () => (
       src={introVideo}
     ></video>
   </Box>
-);
+)
 
 const TandC = ({ termsAndCondition }) => (
   <Box>
     <Heading
-      size={'md'}
+      size={"md"}
       children="Terms & Condition"
-      textAlign={['center', 'left']}
+      textAlign={["center", "left"]}
       my="4"
     />
 
-    <Box h="sm" p="4" overflowY={'scroll'}>
+    <Box h="sm" p="4" overflowY={"scroll"}>
       <Text
-        fontFamily={'heading'}
-        letterSpacing={'widest'}
-        textAlign={['center', 'left']}
+        fontFamily={"heading"}
+        letterSpacing={"widest"}
+        textAlign={["center", "left"]}
       >
         {termsAndCondition}
       </Text>
       <Heading
         my="4"
-        size={'xs'}
+        size={"xs"}
         children="Refund only applicable for cancellation within 7 days."
       />
     </Box>
   </Box>
-);
+)
 const About = () => {
   return (
-    <Container maxW={'container.lg'} padding="16" boxShadow={'lg'}>
-      <Heading children="About Us" textAlign={['center', 'left']} />
+    <Container maxW={"container.lg"} padding="16" boxShadow={"lg"}>
+      <Heading children="About Us" textAlign={["center", "left"]} />
       <Founder />
-      <Stack m="8" direction={['column', 'row']} alignItems="center">
-        <Text fontFamily={'cursive'} m="8" textAlign={['center', 'left']}>
+      <Stack m="8" direction={["column", "row"]} alignItems="center">
+        <Text fontFamily={"cursive"} m="8" textAlign={["center", "left"]}>
           We are a video streaming platform with some premium courses available
           only for premium users.
         </Text>
 
         <Link to="/subscribe">
-          <Button variant={'ghost'} colorScheme="yellow">
+          <Button variant={"ghost"} colorScheme="yellow">
             Checkout Our Plan
           </Button>
         </Link>
@@ -98,17 +96,17 @@ const About = () => {
 
       <TandC termsAndCondition={termsAndCondition} />
 
-      <HStack my="4" p={'4'}>
+      <HStack my="4" p={"4"}>
         <RiSecurePaymentFill />
         <Heading
-          size={'xs'}
+          size={"xs"}
           fontFamily="sans-serif"
-          textTransform={'uppercase'}
-          children={'Payment is secured by Razorpay'}
+          textTransform={"uppercase"}
+          children={"Payment is secured by Razorpay"}
         />
       </HStack>
     </Container>
-  );
-};
+  )
+}
 
-export default About;
+export default About
